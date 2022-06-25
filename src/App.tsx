1e4ecf7bffa587298c83a5a7b629b259c2590725
Joao Vitor <joaovitorsocial25@gmail.com>
2022-06-25 00:37:52 -0300

    Criando projeto utilizando React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './paginas/home/Home'
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import { homedir } from 'os';
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src='https://store-images.s-microsoft.com/image/apps.54355.14047496556148589.c22f253d-c9b9-4f11-aee9-75e0ca6ecf73.b40d5201-dd16-4abb-bc50-bd538df83c03' alt="Imagem Tela Inicia - do jogo ori and the will of the wisps." className='img'/>
+        </>
+    );
+}
+
+export default Home;
